9e0c27c8c26e0b8fd2082039418e875ec3afca33
Pascal <pascal@serveurperso.com>
2019-08-20 16:08:38 +0200

    Online3DViewer external dependency

--- stl/index.php
+++ stl/index.php
@@ -6,10 +6,10 @@
    STL
   </title>
 
-  <script src="Online3DViewer/jsmodeler/three.min.js" type="text/javascript"></script>
-  <script src="Online3DViewer/jsmodeler/jsmodeler.js" type="text/javascript"></script>
-  <script src="Online3DViewer/jsmodeler/jsmodeler.ext.three.js" type="text/javascript"></script>
-  <script src="Online3DViewer/embeddable/include/online3dembedder.js" type="text/javascript"></script>
+  <script src="../../Online3DViewer/jsmodeler/three.min.js" type="text/javascript"></script>
+  <script src="../../Online3DViewer/jsmodeler/jsmodeler.js" type="text/javascript"></script>
+  <script src="../../Online3DViewer/jsmodeler/jsmodeler.ext.three.js" type="text/javascript"></script>
+  <script src="../../Online3DViewer/embeddable/include/online3dembedder.js" type="text/javascript"></script>
 
   <meta http-equiv="content-type" content="text/html; charset=utf-8" />
   <meta name="viewport" content="width=device-width, user-scalable=no" />
